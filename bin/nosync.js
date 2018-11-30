7e69d2f02d0e646860140a27277bee2828b7b01b
HaoChuan9421 <hc199421@gmail.com>
2018-11-30 15:19:52 +0800

    添加README

--- bin/nosync.js
+++ bin/nosync.js
@@ -12,177 +12,175 @@ const pwd = process.cwd(); // 当前目录
 const spinner = ora('转化中 🐢 ...\n');
 
 program
-	.version(require('../package').version, '-v, --version')
-	.option('-f, --folder [name]', '禁止同步的[文件夹名]', 'node_modules')
-	.option('-g, --git [boolean]', '是否自动添加 .gitignore')
-	.parse(process.argv);
+  .version(require('../package').version, '-v, --version')
+  .option('-f, --folder [name]', '禁止同步的[文件夹名]', 'node_modules')
+  .option('-g, --git [boolean]', '是否自动添加 .gitignore')
+  .parse(process.argv);
+
+const basePath = path.join(pwd, program.folder); // 用户期望不同步的文件夹的路径，默认是当前目录下的 node_modules
+const nosyncPath = basePath + '.nosync'; // 同名的 nosync 型文件夹的路径
 
 /**
- * 检测项目当前位置是否在 iCloud 中
+ * 检测项目当前位置是否在 iCloud 中（用户并不一定会将桌面和文稿也同步到 iCloud，给个友情提示）
  */
 function checkPwd() {
-	return new Promise((resolve, reject) => {
-		if (/com~apple~clouddocs/i.test(pwd)) {
-			resolve({ continue: true });
-		} else {
-			inquirer
-				.prompt([
-					{
-						type: 'confirm',
-						message: '检测到你当前的项目不在 iCloud 中，是否继续？',
-						name: 'continue'
+  return new Promise((resolve, reject) => {
+    if (/com~apple~clouddocs/i.test(pwd)) {
+      resolve({ continue: true });
+    } else {
+      inquirer
+        .prompt([
+          {
+            type: 'confirm',
+            message: '检测到你当前的项目不在 iCloud 中，是否继续？',
+            name: 'continue'
 					}
 				])
-				.then(answers => {
-					answers.continue ? resolve(answers) : reject(answers);
-				})
-				.catch(error => {
-					reject(error);
-				});
-		}
-	});
+        .then(answers => {
+          answers.continue ? resolve(answers) : reject(answers);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    }
+  });
 }
 
 /**
  * 创建 .nosync 文件夹
  */
-function createFolder() {
-	const basePath = path.join(pwd, program.folder);
-	const nosyncPath = basePath + '.nosync';
-	const exitBase = fs.existsSync(basePath);
-	const exitNosync = fs.existsSync(nosyncPath);
+function createNosyncFolder() {
+  spinner.start();
+  const baseExist = fs.existsSync(basePath); // 期望的路径是否已占用
+  const nosyncExist = fs.existsSync(nosyncPath); // 期望的 nosync 路径是否已占用
+  const baseIsDirectory = baseExist && fs.lstatSync(basePath).isDirectory(); // 期望的路径是否已存在文件夹
 
-	return new Promise((resolve, reject) => {
-		// 1. 已经同时存在两种类型的文件夹，无需额外操作了
-		if (exitBase && exitNosync) {
-			console.log(chalk.black.bgRed('\n你多虑了!\n'));
-			reject();
-			return;
-		}
-		spinner.start();
-		if (exitBase) { // 2. 指定的文件夹已存在，重命名为 nosync 型
-			fs.rename(basePath, nosyncPath, (err) => {
-				if (err) throw err;
-				resolve({
-					nosyncPath,
-					basePath,
-				});
-			});
-		} else if (exitNosync) { // 3. nosync 型文件夹已存在，直接 resolve 
-			resolve({
-				nosyncPath,
-				basePath
-			});
-		} else { // 4. 都不存在，创建 nosync 型文件夹
-			fs.mkdir(nosyncPath, (err) => {
-				if (err) throw err;
-				resolve({
-					nosyncPath,
-					basePath,
-					install: true
-				});
-			});
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (baseExist && nosyncExist) { // 1. 同时存在，提示用户无需额外操作
+      spinner.stop();
+      console.log(chalk.black.bgRed('\n你多虑了!\n'));
+      reject();
+    } else if (!baseExist && nosyncExist) { // 2. 只存在 nosync，直接 resolve 后制作替身
+      resolve({ install: false });
+    } else if (baseExist && baseIsDirectory) { // 3. 指定的文件夹已存在，重命名为 nosync 型
+      fs.rename(basePath, nosyncPath, (err) => {
+        if (err) throw err;
+        resolve({ install: false });
+      });
+    } else if (baseExist && !baseIsDirectory) { // 4. 之前创建过快捷方式，删除快捷方式并创建空的 nosync 型文件夹
+      fs.unlinkSync(basePath);
+      fs.mkdir(nosyncPath, (err) => {
+        if (err) throw err;
+        resolve({ install: true });
+      });
+    } else { // 5. 都不存在，创建 nosync 型文件夹
+      fs.mkdir(nosyncPath, (err) => {
+        if (err) throw err;
+        resolve({ install: true });
+      });
+    }
+  });
 }
 
 /**
  * 安装 node_modules
  */
 function install() {
-	inquirer
-		.prompt([
-			{
-				type: 'list',
-				message: '请选择安装 node_modules 的方式？',
-				choices: [
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: '请选择安装 node_modules 的方式？',
+        choices: [
 					'yarn',
 					'npm',
 					'cnpm',
 					'暂不安装'
 				],
-				name: 'install'
+        name: 'install'
 			}
 		])
-		.then((res) => {
-			let command = '';
-			switch (res.install) {
-			case 'yarn':
-				command = 'yarn';
-				break;
-			case 'npm':
-				command = 'npm install';
-				break;
-			case 'cnpm':
-				command = 'cnpm install';
-				break;
-			default:
-				break;
-			}
-			// 执行安装命令并输出到控制台
-			command && require('child_process').execSync(command, { stdio: [0, 1, 2] });
-			// 输出成功提示并提示是否将文件夹添加到 .gitignore
-			addGitignore();
-		});
+    .then((res) => {
+      let command = '';
+      switch (res.install) {
+        case 'yarn':
+          command = 'yarn';
+          break;
+        case 'npm':
+          command = 'npm install';
+          break;
+        case 'cnpm':
+          command = 'cnpm install';
+          break;
+        default:
+          break;
+      }
+      // 执行安装命令并输出到控制台
+      command && require('child_process').execSync(command, { stdio: [0, 1, 2] });
+      // 输出成功提示并提示是否将文件夹添加到 .gitignore
+      addGitignore();
+    });
 }
 
 /**
  * 添加忽略规则到 .gitignore
  */
 function addGitignore() {
-	console.log(chalk.green(`\n大功告成，文件夹 ${program.folder} 将不再同步到 iCloud 👏 👏 👏\n`));
+  console.log(chalk.green(`\n大功告成，文件夹 ${program.folder} 将不再同步到 iCloud 👏 👏 👏\n`));
 
-	if (String(program.git) === 'true') {
-		add();
-		return;
-	}
-	if (String(program.git) === 'false') {
-		return;
-	}
-	inquirer
-		.prompt([
-			{
-				type: 'confirm',
-				message: `是否添加 ${program.folder}* 到 .gitignore？`,
-				name: 'git'
+  // 如果用户已通过命令行指定是否添加到 git，则不再提示
+  if (String(program.git) === 'true') {
+    add();
+    return;
+  }
+  if (String(program.git) === 'false') {
+    return;
+  }
+  // 提示用户是否添加忽略规则到 .gitignore
+  inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        message: `是否添加 ${program.folder}* 到 .gitignore？`,
+        name: 'add'
 			}
 		])
-		.then(answers => {
-			answers.git && add();
-		});
-	// 执行添加操作
-	function add() {
-		const gitPath = path.join(pwd, '.gitignore');
-		const exitGit = fs.existsSync(gitPath);
-		if (exitGit) {
-			fs.appendFile('.gitignore', `\n${program.folder}*\n`, (err) => {
-				if (err) throw err;
-				console.log(chalk.green('\n添加 .gitignore 成功\n'));
-			});
-		} else {
-			fs.writeFile('.gitignore', `.DS_Store\n${program.folder}*\n`, (err) => {
-				if (err) throw err;
-				console.log(chalk.green('\n添加 .gitignore 成功\n'));
-			});
-		}
-	}
+    .then(answers => {
+      answers.add && add();
+    });
+  // 执行添加操作
+  function add() {
+    const gitPath = path.join(pwd, '.gitignore');
+    const gitExist = fs.existsSync(gitPath);
+    if (gitExist) {
+      fs.appendFile('.gitignore', `\n${program.folder}*\n`, (err) => {
+        if (err) throw err;
+        console.log(chalk.green('\n添加 .gitignore 成功\n'));
+      });
+    } else {
+      fs.writeFile('.gitignore', `.DS_Store\n${program.folder}*\n`, (err) => {
+        if (err) throw err;
+        console.log(chalk.green('\n添加 .gitignore 成功\n'));
+      });
+    }
+  }
 }
 
 checkPwd()
-	.then(() => createFolder())
-	.then(res => {
-		// 检测过文件位置并创建 nosync 型文件夹后 制作替身
-		fs.symlinkSync(res.nosyncPath, res.basePath, 'file');
-		// 结束进程指示器
-		spinner.stop();
-		// 如果 指定的 nosync 文件夹是 node_modules 并且 之前不存在 node_modules，则提示用户安装
-		if (program.folder === 'node_modules' && res.install) {
-			install();
-		} else {
-			// 输出成功提示并提示是否将文件夹添加到 .gitignore
-			addGitignore();
-		}
-	})
-	.catch(() => {
-		process.exit();
-	});
+  .then(() => createNosyncFolder())
+  .then(res => {
+    // 为 nosync 文件夹制作替身
+    fs.symlinkSync(nosyncPath, basePath, 'file');
+    // 结束进程指示器
+    spinner.stop();
+    // 如果 指定的 nosync 文件夹是 node_modules 并且 之前不存在 node_modules，则提示用户安装
+    if (program.folder === 'node_modules' && res.install) {
+      install();
+    } else {
+      // 输出成功提示并提示是否将文件夹添加到 .gitignore
+      addGitignore();
+    }
+  })
+  .catch(() => {
+    process.exit();
+  });
---
+++ docs/README_en.md
@@ -0,0 +1,17 @@
+# nosync-icloud
+
+> Avoid `node_modules` to sync with `iCloud`.
+
+<p align="center">
+    <a href="https://www.npmjs.com/package/nosync-icloud"><img src="https://img.shields.io/npm/v/nosync-icloud.svg" alt="Version"></a>
+    <a href="https://npmcharts.com/compare/nosync-icloud?minimal=true"><img src="https://img.shields.io/npm/dm/nosync-icloud.svg" alt="Downloads"></a>
+    <a href="https://github.com/HaoChuan9421/nosync-icloud/commits/master"><img src="https://img.shields.io/github/last-commit/haochuan9421/nosync-icloud.svg" alt="Commit"></a>
+    <a href="https://github.com/HaoChuan9421/nosync-icloud/issues"><img src="https://img.shields.io/github/issues-closed/haochuan9421/nosync-icloud.svg" alt="Issues"></a>
+    <a href="https://www.npmjs.com/package/vue-ueditor-wrap"><img src="https://img.shields.io/npm/l/vue-ueditor-wrap.svg" alt="License"></a>
+</p>
+
+[简体中文](https://github.com/HaoChuan9421/nosync-icloud/blob/master/docs/README_zh.md) [English](https://github.com/HaoChuan9421/nosync-icloud/blob/master/docs/README_en.md)
+
+<img src="https://github.com/HaoChuan9421/nosync-icloud/raw/master/assets/vs.png" />
+
+### Writing...
